5966b6a1545250adb72fa8db12a1594e9731fcb2
tuan.nq <tuan.nq@flexidata.vn>
2017-09-29 18:20:21 +0700

    feat: 20170929-0620

--- MomShopping/src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ MomShopping/src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,20 @@
+import { Recipe } from './recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+	selector: 'app-recipe-list',
+	templateUrl: './recipe-list.component.html',
+	styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+	recipes: Recipe[] = [
+		new Recipe('A test recipe', 'Testing', 'https://static.pexels.com/photos/76093/pexels-photo-76093.jpeg')
+	];
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
 
 }
